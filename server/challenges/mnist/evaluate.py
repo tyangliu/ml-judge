3b72f9268bc610a752a4bfcedd05d6050eb8577b
Thomas Liu <tyangliu@gmail.com>
2017-11-24 15:52:18 -0800

    Test

--- server/challenges/mnist/evaluate.py
+++ server/challenges/mnist/evaluate.py
@@ -1,8 +1,11 @@
 import numpy as np
+import zipfile
 
-def evaluate(pred_csv):
+def evaluate(path):
+    archive = zipfile.ZipFile(path, 'r')
+
     test_labels = np.array([int(e) for e in open("test_labels.csv")])
-    pred_labels = np.array([int(e) for e in open(pred_csv)])
+    pred_labels = np.array([int(e) for e in archive.open("predictions.csv")])
 
     assert len(test_labels) == len(pred_labels), "Must have correct number of samples"
     score = 100 * (1 - sum(test_labels == pred_labels)/len(test_labels))
