baf18c3672e2b6ce6d290054a43a25d78185e663
Thomas Liu <tyangliu@gmail.com>
2018-02-01 16:08:01 -0800

    Update salaries challenge evaluate/submission/test set

--- server/challenges/salaries/evaluate.py
+++ server/challenges/salaries/evaluate.py
@@ -2,10 +2,8 @@ import numpy as np
 import zipfile
 
 def evaluate(path):
-    archive = zipfile.ZipFile(path, 'r')
-
-    test_labels = np.array([int(e) for e in open("challenges/titanic/test_labels.csv")])
-    pred_labels = np.array([int(e) for e in archive.open("predictions.csv")])
+    test_labels = np.array([int(e) for e in open("challenges/salaries/test.csv")])
+    pred_labels = np.array([int(e) for e in open(path)])
 
     assert len(test_labels) == len(pred_labels), "Must have correct number of samples"
     score = 100 * (sum(test_labels == pred_labels)/len(test_labels))
--- server/challenges/salaries/submission.md
+++ server/challenges/salaries/submission.md
@@ -1,4 +1,1 @@
-Your submission should be a zip file containing:
-* src/ - directory containing your source code files
-* predictions.csv - your predictions for the test set.
-* readme.txt - text file with a brief summary of your approach and model.
+Your submission should be a single csv file containing your test set predictions.
